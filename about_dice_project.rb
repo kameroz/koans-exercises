b6a8a2ea00672dc03556d1ae78144a875bdc7f06
kameroz <cameros_azul@hotmail.com>
2015-10-28 14:34:21 -0600

    sexto avance koans

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,13 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+  def roll(n)
+    @values = (1..n).map { rand(6) + 1 }
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
@@ -47,7 +54,7 @@ class AboutDiceProject < Neo::Koan
     #
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
-    # better way to test this?
+    # better way to test this.
   end
 
   def test_you_can_roll_different_numbers_of_dice
