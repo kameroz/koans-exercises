91e61ed9980d7b0e9ed07a8eace5821982d707f4
kameroz <cameros_azul@hotmail.com>
2015-10-28 12:42:58 -0600

    quinto avance koans

--- about_triangle_project_2.rb
+++ about_triangle_project_2.rb
@@ -6,11 +6,18 @@ require './triangle.rb'
 class AboutTriangleProject2 < Neo::Koan
   # The first assignment did not talk about how to handle errors.
   # Let's handle that part now.
-  def test_illegal_triangles_throw_exceptions
-    assert_raise(TriangleError) do triangle(0, 0, 0) end
-    assert_raise(TriangleError) do triangle(3, 4, -5) end
-    assert_raise(TriangleError) do triangle(1, 1, 3) end
-    assert_raise(TriangleError) do triangle(2, 4, 2) end
-    # HINT: for tips, see http://stackoverflow.com/questions/3834203/ruby-koan-151-raising-exceptions
-  end
+   def test_illegal_triangles_throw_exceptions
+    assert_raise(TriangleError) do triangle(0, 0, 0) 
+      raise TriangleError.new("Triangle values must be over one.")
+    end
+    assert_raise(TriangleError) do triangle(3, 4, -5)
+      raise TriangleError.new("Triangle values must be greater than  one.")
+    end
+    assert_raise(TriangleError) do triangle(1, 1, 3) 
+      raise TriangleError.new("Sum of two sides must be greater than the third side.")
+    end
+    assert_raise(TriangleError) do triangle(2, 4, 2) 
+      raise TriangleError.new("Sum of two sides must be greater than the third side.")
+    end
+ end
 end
